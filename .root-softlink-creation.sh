f4d954bef6b0cab07695b9dd70df26b2532f5cac
Javier Fraga <javier.s.fraga@gmail.com>
2016-09-17 19:10:24 -0500

    adds .root-softlink-creation.sh

---
+++ .root-softlink-creation.sh
@@ -0,0 +1,24 @@
+links=(
+	".alias"
+	".bashrc"
+	".git-completion.bash"
+	".git-prompt.sh"
+	".gitrc"
+	".mvn-color.sh"
+	".mvn-color.sh"
+	".vim"
+	".vimrc"
+	".config/nvim/UltiSnips"
+	".config/nvim/init.vim"
+	)
+# make .config/nvim directory
+if [ ! -d "/root/.config/nvim" ]; then
+	mkdir -p /root/.config/nvim
+fi
+# make links
+for i in "${links[@]}"; do
+	if [[ ! -e "/root/$i" ]]; then
+		ln -s /home/javier/$i /root/$i
+	fi
+done
+
