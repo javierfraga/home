419d92c8a96813b4b4f4b9919ec5ecbcb4325e8c
Javier Fraga <javier.s.fraga@gmail.com>
2017-01-10 07:04:56 -0600

    last commit before dies

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -84,7 +84,7 @@ end
 -- Define a tag table which hold all screen tags.
 --tags = {}
 tags = {
-	names  = { "docker.1", "mongo.2", "blank.3", "vb.4", "py.5", "slack.6", "c.6", "torr.8", "msg.9" },
+	names  = { "docker.1", "mongo.2", "lcthw.3", "youtube.4", "py.5", "gatech.6", "c.7", "torr.8", "msg.9" },
 	layout = { 
 		layouts[1], layouts[2], layouts[1], layouts[5], layouts[6],
 		layouts[12], layouts[9], layouts[3], layouts[7]
